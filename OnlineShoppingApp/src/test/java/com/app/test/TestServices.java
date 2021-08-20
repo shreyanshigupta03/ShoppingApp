edf55e10cf11edbd5fe908083436e7ad37b6fa19
shreyanshi gupta <Shreyanshigupta03@gmail.com>
2021-08-20 14:00:33 +0530

    junit

---
+++ OnlineShoppingApp/src/test/java/com/app/test/TestServices.java
@@ -0,0 +1,139 @@
+package com.app.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TestServices {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testGetCustomLoginDao() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testValidate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCustomerLoginService() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetProductByName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetProductByPrice() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAddProductToCart() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testViewCart() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testPlaceOrder() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRegisterCustomer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testTotalInCart() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetClass() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testHashCode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEquals() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testClone() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testNotify() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testNotifyAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testWaitLong() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testWaitLongInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testWait() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFinalize() {
+		fail("Not yet implemented");
+	}
+
+}
